7f2ed4bc8e935d885fc54cce42a51c7c1903e7c5
Mr.xun <969156467@qq.com>
2018-03-05 09:17:50 +0800

    seven

--- src/components/SelectSession.js
+++ src/components/SelectSession.js
@@ -2,35 +2,66 @@ import React,{Component} from "react";
 import axios from "axios";
 import {NavLink} from "react-router-dom";
 import "../style/selectSession.scss";
-// import "../js/swiper.min.js"
+import $ from 'jquery';
 import Swiper from 'swiper'
 import "../css/swiper.min.css"
 export default class SelectSession extends Component{
 	constructor(props){
 		super(props);
 		this.state={
-			bannerImg:[]
+			bannerImg:[],
+			currentIndex:0,
+			filmName:"",
+			schedules:[]
 		}
+	}
+	getTime(id,currentIndex){
+		var that = this;
+		axios.get(`/v4/api/schedule?__t=1520134799929&film=0&cinema=${id}`)
+		.then((res)=>{
+			this.setState({schedules:res.data.data.schedules},function(){
+				var schedules = that.state.schedules;
+				schedules.map(function(item,index){
+					if(`${item.film.id}` === currentIndex){
+						console.log(item)
+					}
+				})
+			})
+		})
 	}
 	componentDidMount(){
 		var that = this;
-		axios.get("/v4/api/cinema/5145/film?__t=1520069232668")
+		var  id = this.props.match.params.fid;
+		console.log(id)
+		axios.get(`/v4/api/cinema/${id}/film?__t=1520069232668`)
 		.then((res)=>{
-			console.log(res)
 			this.setState({bannerImg:res.data.data.filmList},function(){
-				new Swiper(that.refs.swiper, {
-				autoplay: true,//可选选项，自动滑动
-				freeMode : true,
-				freeModeMomentum : true,
-				slidesPerView: 'auto',
-				oopedSlides: 8,
+				var currentIndex  = res.data.data.filmList[0].filmID;
+				var currentFilmName = res.data.data.filmList[0].filmName;
+				that.setState({currentIndex:currentIndex,filmName:currentFilmName})
+				that.getTime(id,currentIndex)
+				var mySwiper = new Swiper(that.refs.swiper, {
+				slidesPerView : 4,
+				centeredSlides : true,
+				slideToClickedSlide: true,
+				on: {
+				    slideChangeTransitionEnd: function(){
+				    	currentIndex = $('.swiper-slide-active').attr("data-index");
+				    	currentFilmName = $('.swiper-slide-active').attr("data-name")
+				    	that.setState({currentIndex:currentIndex,filmName:currentFilmName},function(){
+							that.getTime(id,currentIndex)
+				    	})
+				    }
+				  }
 				})
 			})
 		})
 		
 	}
 	render(){
-		console.log(this.state.bannerImg)
+		console.log(this.state)
+		var currentIndex = this.state.currentIndex;
+		var filmName = this.state.filmName;
 		return(
 			<div className="selectSession">
 				<div className="banner">
@@ -38,20 +69,24 @@ export default class SelectSession extends Component{
 					  <div className="swiper-wrapper">
 					  	{this.state.bannerImg.map(function(item,index){
 					  		return(
-					  			<div className="swiper-slide">
-					  				<div className="imgWrap">
+					  			<div className="swiper-slide" data-index={item.filmID} data-name={item.filmName} key={item.filmID}>
+					  				<div className={currentIndex == item.filmID? "imgWrap active_img" : "imgWrap"}>
 					  					<div className="img">
-					  						<img src={item.posterAddress}/>
+					  						<img  src={item.posterAddress}/>
 					  					</div>
 					  				</div>
-					  				<div className="film_name">{item.filmName}</div>
+					  				<div className={currentIndex == item.filmID? "film_name active_name" : "film_name"}>{filmName}</div>
 					  			</div>
 					  		)
 					  	})}
 					  </div>
 					</div>
 				</div>
-				
+				<div className="chooseTime">
+					<div className="chooseTime_title">
+						
+					</div>
+				</div>
 			</div>
 		)
 	}
